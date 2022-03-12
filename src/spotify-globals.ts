5d2a43842061ce84c0d289486063d63a1902b8ad
conejerock <jjcs2@alu.ua.es>
2022-03-12 22:47:01 +0100

    Changing callback docker

--- src/spotify-globals.ts
+++ src/spotify-globals.ts
@@ -6,21 +6,21 @@ const CURRENT_SONG_NAME_FILE: string = "current.json";
 const SVG_SRC_FILE: string = "src.svg";
 const SVG_DST_FILE: string = "dst.svg";
 
-const EXPRESS_HOSTNAME: string = process.env.EXPRESS_HOSTNAME || "localhost";
 const EXPRESS_PORT: number = Number(process.env.EXPRESS_PORT) || 80;
-
-const EXPRESS_CREATE_TOKEN_PORT: number = Number(process.env.EXPRESS_CREATE_TOKEN_PORT) || 8888;
-const EXPRESS_CALLBACK: string = process.env.EXPRESS_CALLBACK || "callback";
 const EXPRESS_SVG_PATH: string = process.env.EXPRESS_SVG_PATH || "current.svg";
+const EXPRESS_HOSTNAME: string = process.env.EXPRESS_HOSTNAME || "localhost";
 const EXPRESS_HOST: string = `http://${EXPRESS_HOSTNAME}${EXPRESS_PORT ? `:${EXPRESS_PORT}` : ""}`;
 
+const EXPRESS_CALLBACK: string = process.env.EXPRESS_CALLBACK || "callback";
+const EXPRESS_CREATE_TOKEN_PORT: number = Number(process.env.EXPRESS_CREATE_TOKEN_PORT) || 8888;
+
+const SPOTIFY_LIST_SCOPES: string[] = ["user-read-currently-playing"];
 const SPOTIFY_CLIENT_ID: string = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET_ID: string = process.env.SPOTIFY_CLIENT_SECRET_ID;
-const SPOTIFY_LIST_SCOPES: string[] = ["user-read-currently-playing"];
-const SPOTIFY_CALLBACK: string = `${EXPRESS_HOST}:${EXPRESS_CREATE_TOKEN_PORT}/${EXPRESS_CALLBACK}`;
+const SPOTIFY_CALLBACK: string = `http://${EXPRESS_HOSTNAME}:${EXPRESS_CREATE_TOKEN_PORT}/${EXPRESS_CALLBACK}`;
 
-if(!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET_ID) {
-  throw new Error("SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET_ID are mandatory in .env file")
+if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET_ID) {
+  throw new Error("SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET_ID are mandatory in .env file");
 }
 
 const URL_SVG = `${EXPRESS_HOST}/${EXPRESS_SVG_PATH}`;
